4d4ed7d33ba7f88bdedf0f4076176ae901ea6b38
akshayghuge9990 <ghugeakshay123@gmail.com>
2023-07-10 09:27:23 +0530

    changes in userTest class and productTest class

--- ElectronicStore/src/test/java/com/electronicstore/controller/UserControllerTest.java
+++ ElectronicStore/src/test/java/com/electronicstore/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.electronicstore.controller;
 
 import com.electronicstore.dtos.UserDto;
 import com.electronicstore.entity.User;
+import com.electronicstore.model.ImageResponse;
 import com.electronicstore.model.PageableResponse;
 import com.electronicstore.serviceI.UserServiceI;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,6 +19,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +30,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserControllerTest {
     @MockBean
     private UserServiceI userServiceI;
+
+    @Autowired
+    private UserController userController;
 
 
     @Autowired
@@ -63,7 +69,7 @@ class UserControllerTest {
                 .email("akshayghuge8080@gmail.com")
                 .gender("male")
                 .password("Akshay@123")
-                .imageName("Akshay.png")
+                .imageName("687aee31-4e2b-42e4-8b76-e92ab9188980.png")
                 .build();
 
         user1 = User.builder()
@@ -146,6 +152,9 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.name").exists());
 
     }
+
+    // when the request send to server then object will be converted into json form
+    // this method is used ony for the  convertting  one object to json format
 
     private String convertObjectTOJsonString(Object user) {
 
@@ -232,6 +241,8 @@ class UserControllerTest {
 
     }
 
+    //delete
+
     @Test
     void deleteUserTest() throws Exception {
 
@@ -254,6 +265,8 @@ class UserControllerTest {
 
 
     }
+
+   // find by Email Id
 
     @Test
     void findByEmailTest() throws Exception {
@@ -278,15 +291,52 @@ class UserControllerTest {
 
     }
 
+    //search Name
     @Test
-    void searchNameTest() {
+    void searchNameTest() throws Exception {
+
+        //  /api/users/search/{keyword}+get request+json
+
+        String name="Akshay";
 
 
+        List<UserDto> dtoList = userList.stream().map((u) -> modelMapper.map(u, UserDto.class)).collect(Collectors.toList());
+
+        Mockito.when(userServiceI.searchUser(Mockito.any())).thenReturn(dtoList);
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/users/search/"+name)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andDo(print())
+                .andExpect(status().isOk());
 
     }
 
+    //upload Image(Problem!!!!!!!!!!!!)
+
     @Test
-    void uploadUserImageTest() {
+    void uploadUserImageTest() throws Exception {
+
+        //   /api/users/image/{userId}+POST request+json
+
+        String userId="1234";
+
+
+        UserDto dto = modelMapper.map(user, UserDto.class);
+
+        Mockito.when(userServiceI.updateUser(Mockito.any(),Mockito.anyString())).thenReturn(dto);
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/users/image/"+userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(convertObjectTOJsonString(user))
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.imageName").exists());
+
+
 
 
 
